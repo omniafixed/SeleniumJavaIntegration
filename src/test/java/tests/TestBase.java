7202ff40ee52618e464e13497e6f25051fe1cf3d
Omnia Tarek <omnia.tarek@fixedmea.com>
2019-06-15 22:39:14 +0200

    downloads file

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -28,21 +28,13 @@ public class TestBase
 
 	public static FirefoxOptions firefoxOption() {
 		FirefoxOptions option = new FirefoxOptions();
-		option.addPreference("browser.download.folderList", 2);
-		option.addPreference("browser.download.dir", downloadPath);
-		option.addPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf");
-		option.addPreference("browser.download.manager.showWhenStarting", false);
-		option.addPreference("pdfjs.disabled", true);
+	
 		return option;
 	}
 
 	public static ChromeOptions chromeOption() {
 		ChromeOptions options = new ChromeOptions();
-		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-		chromePrefs.put("profile.default.content_settings.popups", 0);
-		chromePrefs.put("download.default_directory", downloadPath);
-		options.setExperimentalOption("prefs", chromePrefs);
-		options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		
 		return options;
 	}
 
@@ -51,24 +43,15 @@ public class TestBase
 	public void startDriver(@Optional("chrome") String browserName) 
 	{
 		if (browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+
 			driver = new ChromeDriver(chromeOption()); 
 		}
 
 		else if(browserName.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
+
 			driver = new FirefoxDriver(firefoxOption()); 
 		}
 
-		else if (browserName.equalsIgnoreCase("ie")) 
-		{
-			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"/drivers/IEDriverServer.exe");
-			driver = new InternetExplorerDriver(); 
-		}
-
-		else if (browserName.equalsIgnoreCase("safari")) {
-			driver = new SafariDriver(); 
-		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 		driver.navigate().to("http://demo.nopcommerce.com/");
